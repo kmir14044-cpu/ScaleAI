ca9ac7720864cd9d4b4b093eab59bb85184758a1
Your Name <your@email.com>
2026-05-10 23:15:22 -0700

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,11 @@ rembg_session = new_session("u2netp")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+    allow_origins=[
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "https://scale-ai-5k3b.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
